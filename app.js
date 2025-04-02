2101b2bfca812dd3719d8d2b85c812baa987f0de
Igrcic12 <85997496+Igrcic12@users.noreply.github.com>
2025-04-02 18:04:09 +0200

    just some fixes

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
 let count = 0;
+//prikaz countera na UI
+document.getElementById("counter").textContent = count;
+
 
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const reset = document.getElementById("reset");
 const di = document.getElementById("counter");
 
+// counter plus 1
 plus.addEventListener("click", function(){
     count++;
     di.textContent = count;
     if(count > 0){
         counter.style.color = "black";
     }
-    else{
-        counter.style.color = "red";
-    }
+
 })
 
+//counter minus 1
 minus.addEventListener("click", function(){
     count--;
     di.textContent = count;
-    if(count < 0){
+    if(count <= -1){
         counter.style.color = "red";
     }
-    else{
-        counter.style.color = "black";
-    }
-})
 
+})
+// resetiranje countera
 reset.addEventListener("click", function(){
     count = 0;
     di.textContent = count;
